51b0c9b282c0dfe80f9430cbaf1c3d32a8ffd602
Brooke Weingard <bweinga2@jh.edu>
2025-09-29 23:42:41 -0400

    exercise 3 python update for second part

--- week3/ex3.py
+++ week3/ex3.py
@@ -3,7 +3,7 @@
 sample_ids = ["A01_62", "A01_39", "A01_63", "A01_35", "A01_31",
               "A01_27", "A01_24", "A01_23", "A01_11", "A01_09"]
 
-z = open("genotype.txt","w")
+z = open("gt_long.txt","w")
 for line in open('biallelic.vcf'):
     if line.startswith('#'):
         continue
@@ -20,6 +20,27 @@ for line in open('biallelic.vcf'):
             print ("1", file = z)
         else:
             continue
-
+        print()
 #Q3.1 I think that A01_09, A01_11, A01_39 are definetly derived from lab strain, as they have no full variant calls
 #A01_24, A01_27, A01_31, and A01_65 have the same insertional mutations so maybe they could be derived from wine strain.
+
+a = open("Chr2_A01_62.txt","w")
+for line in open('biallelic.vcf'):
+    if line.startswith('#'):
+        continue
+    fields = line.rstrip('\n').split('\t')
+    chrom = fields[0]
+    pos = fields[1]
+    samples = fields[9:]
+    if chrom == "II":
+        for i in range(len(sample_ids)):
+                if sample_ids[i] == "A01_62":
+                    sample_data = samples[i]
+                genotype = sample_data.split(":")[0]
+                if genotype == "0":
+                    print("0", file = a)
+                elif genotype == "1":
+                    print ("1", file = a)
+                else:
+                    continue
+                print()
